e24a2c2eecc4a6bf0fb296c5118718717565a081
yangyili <1156612970@qq.com>
2014-03-31 09:51:25 +0800

    implement add work

--- todo/js/todo.js
+++ todo/js/todo.js
@@ -1,5 +1,12 @@
 var Todo = function() {
-    var add_work = function(target) {};
+    var add_work = function(work) {
+        var work_element = '<li class="active">' +
+            '<a class="sel" href="javascript:{}">sign</a>' +
+            '<span>' + work + '</span>' +
+            '<a class="del" href="javascript:{}">x</a>'
+        '</li>';
+        $('.works ul').append($(work_element));
+    };
     return {
         add_work: add_work
     };
