e7e49d49ea0f508eb0e6c78ca08e70150ca0218d
yangyili <1156612970@qq.com>
2014-04-03 11:38:21 +0800

    seperate view and models

--- todo/js/todo4.js
+++ todo/js/todo4.js
@@ -1,75 +1,63 @@
 var Todo4 = function () {
-    var init_data = function () {
-        var all_works = store.get('all_works') || [];
+    var all_works = store.get('all_works') || [];
+    function init() {
+        $('input').on('keypress', function (event) {
+            if (event.keyCode == 13 && $(this).val()) {
+                add_work($(this).val());
+                $(this).val('');
+            }
+        });
+        render();
+    }
+
+    var render = function () {
         $('.works ul').empty();
         for (var i = 0; i < all_works.length; i++) {
-            render_one(all_works[i]);
+            render_one(all_works[i], i);
         }
     };
-    var render_one = function (work) {
+    var render_one = function (work, index) {
         var li_class = work.is_complete ? 'complete' : 'active';
-        var id = work.id.toString();
         var li = '<li class='+ li_class +'>' +
-            '<a href="javascript:{}" class="sel" data-id='+ work.id +'>s</a>' +
+            '<a href="javascript:{}" class="sel">s</a>' +
             '<span>' + work.name + '</span>' +
-            '<a href="javascript:{}" class="del" data-id='+ work.id + '>x</a>' +
+            '<a href="javascript:{}" class="del">x</a>' +
             '</li>';
         var $ul = $('.works ul');
         var $li = $(li);
         $ul.append($li);
         $li.find('.del')
             .click(function () {
-                var id = $(this).data('id');
-                Todo4.remove_work(id);
+                remove_work(index);
             });
 
         $li.find('.sel')
             .click(function () {
-                var id = $(this).data('id');
-                Todo4.toggle_work(id);
+                toggle_work(index);
             });
-        ;
-
     };
 
     var add_work = function (name) {
-        var works = store.get('all_works') || [];
-        var id = works.length + 1;
-        var work = {id: id.toString(), is_complete: 0, name: name};
-        works.unshift(work);
-        store.set('all_works', works);
-        init_data();
+        var work = {is_complete: 0, name: name};
+        all_works.unshift(work);
+        store.set('all_works', all_works);
+        render();
     };
 
-    var remove_work = function (id) {
-        var index = index_of_works(id);
-        var works = store.get('all_works');
-        works.splice(index, 1);
-        store.set('all_works', works);
-        init_data();
+    var remove_work = function (index) {
+        all_works.splice(index, 1);
+        store.set('all_works', all_works);
+        render();
     };
 
-    var toggle_work = function (id) {
-        var index = index_of_works(id);
-        console.log('index', index);
-        var works = store.get('all_works') || {};
-        works[index].is_complete = !works[index].is_complete;
-        store.set('all_works', works);
-        init_data();
-    };
-
-    var index_of_works = function (id) {
-        var works = store.get('all_works');
-        for (var i = 0; i < works.length; i++) {
-            if (works[i].id == id) {
-                return i;
-            }
-        }
-        return -1;
+    var toggle_work = function (index) {
+        all_works[index].is_complete = !all_works[index].is_complete;
+        store.set('all_works', all_works);
+        render();
     };
 
     return {
-        init: init_data,
+        init: init,
         add_work: add_work,
         remove_work: remove_work,
         toggle_work: toggle_work
