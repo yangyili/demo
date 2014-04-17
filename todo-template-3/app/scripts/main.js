ba8cac662954fe6752fd6e62bec68db4e446b734
yangyili <1156612970@qq.com>
2014-04-17 09:52:45 +0800

    implement add work

--- todo-template-3/app/scripts/main.js
+++ todo-template-3/app/scripts/main.js
@@ -1,1 +1,17 @@
-console.log('\'Allo \'Allo!');
+var Todo7 = function () {
+    var all_work = [];
+    function init() {
+        $('input.unadded-todo').keypress(function (event) {
+            var $this = $(this);
+            if (event.keyCode == 13 && $this.val()) {
+                var work = {name: $this.val(), is_complete: 0};
+                all_work.unshift(work);
+                $this.val('');
+            }
+        });
+    }
+
+    return {
+        init: init
+    };
+}();
